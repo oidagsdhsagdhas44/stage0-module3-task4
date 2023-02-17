bdfc98901cad90b69fab35ebdafe759d6fce4f60
oidagsdhsagdhas44 <tornike.buchukuri@gau.edu.ge>
2023-02-18 03:46:57 +0400

    first try

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        float a=0, b=0;
+        float res = (9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2);
+        System.out.println(res);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 'c', h = 'h', a = 'a', r = 'r';
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
     }
 }
